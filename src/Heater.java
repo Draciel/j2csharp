1268085d23b34bfe40643990a0621a96d4c807a7
Jakub Tyrka <jakub.t.tyrka@gmail.com>
2019-04-03 16:55:32 +0200

    Add sample code

---
+++ src/Heater.java
@@ -0,0 +1,28 @@
+class Heater {
+
+    private final int maxTemp;
+
+    private int currentTemp = 0;
+
+    private Heater(final int maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public static Heater create(final int maxTemp) {
+        return new Heater(maxTemp);
+    }
+
+    public void heat(final int offset) {
+        if ((maxTemp - currentTemp - offset) > 0) {
+            currentTemp += offset;
+        }
+        System.out.println("Heating " + currentTemp);
+    }
+
+    public void cool(final int offset) {
+        if (currentTemp - offset > 0) {
+            currentTemp -= offset;
+        }
+        System.out.println("Cool " + currentTemp);
+    }
+}
